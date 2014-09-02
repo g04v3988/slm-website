595c85b7ba6b2bde8d4f2d71eebcd5245b264548
g04v3988 <g04v3988@gmail.com>
2014-09-02 21:03:50 +0200

    Updated hompage
    Responsive design implemented on homepage. Still to do the media
    queries.

--- SpreadLuv Website/js/carousel.js
+++ SpreadLuv Website/js/carousel.js
@@ -1,7 +1,8 @@
 ﻿$(document).ready(function(){
 	// Set the interval to be 5 seconds
-	var t = setInterval(function(){
-		$("#carousel ul").animate({marginLeft:-1063},2500,function(){
+    var t = setInterval(function () {
+        var width = $("#carousel ul li").innerWidth;
+		$("#carousel ul").animate({marginLeft:width},2500,function(){
 			$(this).find("li:last").after($(this).find("li:first"));
 			$(this).css({marginLeft:0});
 		})
