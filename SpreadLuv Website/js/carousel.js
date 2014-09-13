498b4f7483d2caaa188c90630003db14dc5997f3
g04v3988 <g04v3988@gmail.com>
2014-09-13 16:45:45 +0200

    Responsive Menu Added
    Added responsive menu

--- SpreadLuv Website/js/carousel.js
+++ SpreadLuv Website/js/carousel.js
@@ -1,10 +1,22 @@
 ﻿$(document).ready(function(){
-	// Set the interval to be 5 seconds
-    var t = setInterval(function () {
+	// Set the interval to be 7.5 seconds
+    var t = setInterval(MoveNext, 7500);
+
+    //$("#carousel ul li").on("swipe", MoveNext);
+
+    function MoveNext() {
         var width = $("#carousel ul li").innerWidth;
-		$("#carousel ul").animate({marginLeft:width},2500,function(){
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
-	},7500);
+        $("#carousel ul").animate({ marginLeft: -width }, 2500, function () {
+            $(this).find("li:last").after($(this).find("li:first"));
+            $(this).css({ marginLeft: 0 });
+        })
+    }
+
+    function MovePrevious() {
+        var width = $("#carousel ul li").innerWidth;
+        $("#carousel ul").animate({ marginLeft: width }, 2500, function () {
+            $(this).find("li:last").after($(this).find("li:first"));
+            $(this).css({ marginLeft: 0 });
+        })
+    }
 });
