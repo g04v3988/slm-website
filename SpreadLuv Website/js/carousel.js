a62b3bbd6bfc76e3473b66f0bb2d5a9d8dca6975
chester.cobus <chester.cobus@entelect.co.za>
2014-09-16 23:10:12 +0200

    Added swipe events with jQuery mobile, but the CSS of mobile library is messing up custom CSS (Note : still needs styling fix)

--- SpreadLuv Website/js/carousel.js
+++ SpreadLuv Website/js/carousel.js
@@ -2,9 +2,8 @@
 
 	// Set the interval to be 7.5 seconds
     var t = setInterval(MoveNext, 7500);
-
-    //$("#carousel ul li").on("swipe", MoveNext);
-
+    var ww = document.body.clientWidth;
+    
     function MoveNext() {
         var width = $("#carousel ul li").innerWidth;
         $("#carousel ul").animate({ marginLeft: -width }, 2500, function () {
@@ -18,16 +17,28 @@
             moveFirstAfterLast(this);
         })
     }
-	
-	$('#next').click(function(){
-		 var width = $("#carousel ul li").innerWidth;
-		 var ulCarousel = $("#carousel ul");
-		 moveElement(ulCarousel,-width);
-	});	$('#previous').click(function(){
-		 var width = $("#carousel ul li").innerWidth;
-		 var ulCarousel = $("#carousel ul");
-		 moveElement(ulCarousel,width);
-	});
+    if(ww < 800){
+		$("#carousel ul").on( "swipeleft", function(event) {
+		 	 var width = $("#carousel ul li").innerWidth;
+			 moveElement($(this),-width);
+		});
+		
+		$("#carousel ul").on( "swiperight", function(event) {
+		 	 var width = $("#carousel ul li").innerWidth;
+			 moveElement($(this),width);
+		});
+	}
+	else{
+		$('#next').click(function(){
+			 var width = $("#carousel ul li").innerWidth;
+			 var ulCarousel = $("#carousel ul");
+			 moveElement(ulCarousel,-width);
+		});	$('#previous').click(function(){
+			 var width = $("#carousel ul li").innerWidth;
+			 var ulCarousel = $("#carousel ul");
+			 moveElement(ulCarousel,width);
+		});
+	}
 	
 	function moveElement(element,width){
 		 element.css({ marginLeft: width });
@@ -39,13 +50,5 @@
         $(unorderedList).css({ marginLeft: 0 });
 	}
 	
-	$("#carousel ul").on( "swipeleft", function(event) {
-	 	 var width = $("#carousel ul li").innerWidth;
-		 moveElement($(this),-width);
-	});
 	
-	$("#carousel ul").on( "swiperight", function(event) {
-	 	 var width = $("#carousel ul li").innerWidth;
-		 moveElement($(this),width);
-	});
 });
