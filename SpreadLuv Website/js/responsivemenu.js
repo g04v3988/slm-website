cefec56f9d7e3e245694629e5eba00f2ce571d6c
g04v3988 <g04v3988@gmail.com>
2014-09-21 20:23:27 +0200

    Updated Menu resizing events jquery

--- SpreadLuv Website/js/responsivemenu.js
+++ SpreadLuv Website/js/responsivemenu.js
@@ -1,27 +1,52 @@
 ﻿$(document).ready(function () {
+
     var ww = document.body.clientWidth;
 
-    $(".nav li a").each(function () {
-        if ($(this).next().length > 0) {
-            $(this).addClass("parent");
-        };
-    });
-
-    if (ww < 800) {
-        $(".toggleMenu").click(function (e) {
-            e.preventDefault();
-            $(".nav").toggle();
-        });
-        $(".toggleMenu").css("display", "inline-block");
-        $(".nav li a").click(function () {
-            $(this).parent("li").toggleClass('hover');
-        });
-    } else {
-        $(".toggleMenu").css("display", "none");
-        $(".nav li").hover(function () {
-            $(this).addClass('hover');
-        }, function () {
-            $(this).removeClass('hover');
+    var addParents = function () {
+        $(".nav li a").each(function () {
+            if ($(this).next().length > 0) {
+                $(this).addClass("parent");
+            }
         });
     }
+
+    var adjustMenu = function () {
+        if (ww < 800) {
+            $(".toggleMenu").css("display", "inline-block");
+            if (!$(".toggleMenu").hasClass("active")) {
+                $(".nav").hide();
+            } else {
+                $(".nav").show();
+            }
+            $(".nav li").unbind('mouseenter mouseleave');
+            $(".nav li a.parent").unbind('click').bind('click', function (e) {
+                // must be attached to anchor element to prevent bubbling
+                e.preventDefault();
+                $(this).parent("li").toggleClass("hover");
+            });
+        }
+        else if (ww >= 800) {
+            $(".toggleMenu").css("display", "none");
+            $(".nav").show();
+            $(".nav li").removeClass("hover");
+            $(".nav li a").unbind('click');
+            $(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function () {
+                // must be attached to li so that mouseleave is not triggered when hover over submenu
+                $(this).toggleClass('hover');
+            });
+        }
+    }
+
+    $(".toggleMenu").click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $(this).next(".nav").toggle();
+    });
+    addParents();
+    adjustMenu();
+    
+    $(window).bind('resize orientationchange', function () {
+        ww = document.body.clientWidth;
+        adjustMenu();
+    });
 });
