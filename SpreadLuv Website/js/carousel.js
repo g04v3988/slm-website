0b7a2ec4142385e0ecb54b16c2c2838a60df03ed
g04v3988 <g04v3988@gmail.com>
2014-09-22 11:33:50 +0200

    - Completed menu
     - Implemented Resize event for swipe and click events on image list + re-factored code (animation can still be looked at)
     - Removed JQuery mobile css - (We must roll a theme and then add it back)

--- SpreadLuv Website/js/carousel.js
+++ SpreadLuv Website/js/carousel.js
@@ -1,7 +1,9 @@
-﻿$(document).ready(function(){
+﻿var t = null;
+
+$(document).ready(function(){
 
 	// Set the interval to be 7.5 seconds
-    var t = setInterval(MoveNext, 7500);
+    t = setInterval(MoveNext, 7500);
     var ww = document.body.clientWidth;
     
     function MoveNext() {
@@ -17,38 +19,73 @@
             moveFirstAfterLast(this);
         })
     }
-    if(ww < 800){
-		$("#carousel ul").on( "swipeleft", function(event) {
-		 	 var width = $("#carousel ul li").innerWidth;
-			 moveElement($(this),-width);
-		});
-		
-		$("#carousel ul").on( "swiperight", function(event) {
-		 	 var width = $("#carousel ul li").innerWidth;
-			 moveElement($(this),width);
-		});
-	}
-	else{
-		$('#next').click(function(){
-			 var width = $("#carousel ul li").innerWidth;
-			 var ulCarousel = $("#carousel ul");
-			 moveElement(ulCarousel,-width);
-		});	$('#previous').click(function(){
-			 var width = $("#carousel ul li").innerWidth;
-			 var ulCarousel = $("#carousel ul");
-			 moveElement(ulCarousel,width);
-		});
-	}
 	
-	function moveElement(element,width){
-		 element.css({ marginLeft: width });
-		 moveFirstAfterLast(element);
+    function moveElement(element, width) {
+		element.css({ marginLeft: width });
+        //moveFirstAfterLast(element);
+		//moveLastBeforeFirst(element);
 	}
 	
 	function moveFirstAfterLast(unorderedList){
 		$(unorderedList).find("li:last").after($(unorderedList).find("li:first"));
         $(unorderedList).css({ marginLeft: 0 });
 	}
+
+	function moveLastBeforeFirst(unorderedList) {
+	    $(unorderedList).find("li:first").before($(unorderedList).find("li:last"));
+	    $(unorderedList).css({ marginLeft: 0 });
+	}
 	
+	var adjustEvents = function () {
+	    if (ww < 800) {
+	        $("#carousel ul").on("swipeleft", function (e) {
+	            e.preventDefault();
+	            clearInterval(t);
+	            var width = $("#carousel ul li").innerWidth;
+	            var ulCarousel = $("#carousel ul");
+	            //moveElement(ulCarousel, -width);
+	            moveFirstAfterLast(ulCarousel);
+	            t = setInterval(MoveNext, 7500);
+	        });
+
+	        $("#carousel ul").on("swiperight", function (e) {
+	            e.preventDefault();
+	            clearInterval(t);
+	            var width = $("#carousel ul li").innerWidth;
+	            var ulCarousel = $("#carousel ul");
+	            //moveElement(ulCarousel, width);
+	            moveLastBeforeFirst(ulCarousel);
+	            t = setInterval(MoveNext, 7500);
+	        });
+	    }
+	    else {
+	        $("#carousel ul").off("swiperight");
+	        $("#carousel ul").off("swipeleft");
+	        $('#next').on("click", function (e) {
+	            e.preventDefault();
+	            clearInterval(t);
+	            var width = $("#carousel ul li").innerWidth;
+	            var ulCarousel = $("#carousel ul");
+	            //moveElement(ulCarousel, -width);
+	            moveFirstAfterLast(ulCarousel);
+	            t = setInterval(MoveNext, 7500);
+	        }); $('#previous').on("click", function (e) {
+	            e.preventDefault();
+	            clearInterval(t);
+	            var width = $("#carousel ul li").innerWidth;
+	            var ulCarousel = $("#carousel ul");
+	            //moveElement(ulCarousel, width);
+	            moveLastBeforeFirst(ulCarousel);
+	            t = setInterval(MoveNext, 7500);
+	        });
+	    }
+	}
+
+	adjustEvents();
+
+	$(window).bind('resize orientationchange', function () {
+	    ww = document.body.clientWidth;
+	    adjustEvents();
+	});
 	
 });
