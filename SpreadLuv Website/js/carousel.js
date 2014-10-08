06e31dbe5ee4c624e5ba4e8e59e632cd0b618287
g04v3988 <g04v3988@gmail.com>
2014-10-08 17:47:33 +0200

    Henry - updated gallery page

--- SpreadLuv Website/js/carousel.js
+++ SpreadLuv Website/js/carousel.js
@@ -1,79 +1,60 @@
 ﻿var t = null;
+index = 1;
 
 $(document).ready(function(){
+
+    $('#slide').attr("src", "images/galleries/latest/" + index + ".jpg");
 
 	// Set the interval to be 7.5 seconds
     t = setInterval(MoveNext, 7500);
     var ww = document.body.clientWidth;
     
     function MoveNext() {
-        var width = $("#carousel ul li").innerWidth;
-        $("#carousel ul").animate({ marginLeft: -width }, 2500, function () {
-            moveFirstAfterLast(this);
-        })
+        $('#slide').stop().fadeOut('fast', function () {
+            ++index;
+            if (index == 7)
+                index = 1;
+            $('#slide').attr("src", "images/galleries/latest/" + index + ".jpg");
+            $('#slide').stop().fadeIn('slow');
+        });
     }
 
     function MovePrevious() {
-        var width = $("#carousel ul li").innerWidth;
-        $("#carousel ul").animate({ marginLeft: width }, 2500, function () {
-            moveFirstAfterLast(this);
-        })
+        $('#slide').stop().fadeOut('fast', function () {
+            --index;
+            if (index == 0)
+                index = 6;
+            $('#slide').attr("src", "images/galleries/latest/" + index + ".jpg");
+            $('#slide').stop().fadeIn('slow');
+        });
     }
-	
-    function moveElement(element, width) {
-		element.css({ marginLeft: width });
-        //moveFirstAfterLast(element);
-		//moveLastBeforeFirst(element);
-	}
-	
-	function moveFirstAfterLast(unorderedList){
-		$(unorderedList).find("li:last").after($(unorderedList).find("li:first"));
-        $(unorderedList).css({ marginLeft: 0 });
-	}
-
-	function moveLastBeforeFirst(unorderedList) {
-	    $(unorderedList).find("li:first").before($(unorderedList).find("li:last"));
-	    $(unorderedList).css({ marginLeft: 0 });
-	}
 	
 	var adjustEvents = function () {
 	    if (ww < 800) {
 	        $('#next').off("click");
 	        $('#previous').off("click");
-	        $("#carousel ul").on("swipeleft", function (e) {
+	        $("#carousel img").on("swipeleft", function (e) {
 	            clearInterval(t);
-	            var width = $("#carousel ul li").innerWidth;
-	            var ulCarousel = $("#carousel ul");
-	            //moveElement(ulCarousel, -width);
-	            moveFirstAfterLast(ulCarousel);
+	            MoveNext();
 	            t = setInterval(MoveNext, 7500);
 	        });
 
-	        $("#carousel ul").on("swiperight", function (e) {
+	        $("#carousel img").on("swiperight", function (e) {
 	            clearInterval(t);
-	            var width = $("#carousel ul li").innerWidth;
-	            var ulCarousel = $("#carousel ul");
-	            //moveElement(ulCarousel, width);
-	            moveLastBeforeFirst(ulCarousel);
+	            MovePrevious();
 	            t = setInterval(MoveNext, 7500);
 	        });
 	    }
 	    else {
-	        $("#carousel ul").off("swiperight");
-	        $("#carousel ul").off("swipeleft");
+	        $("#carousel img").off("swiperight");
+	        $("#carousel img").off("swipeleft");
 	        $('#next').on("click", function (e) {
 	            clearInterval(t);
-	            var width = $("#carousel ul li").innerWidth;
-	            var ulCarousel = $("#carousel ul");
-	            //moveElement(ulCarousel, -width);
-	            moveFirstAfterLast(ulCarousel);
+	            MoveNext();
 	            t = setInterval(MoveNext, 7500);
 	        }); $('#previous').on("click", function (e) {
 	            clearInterval(t);
-	            var width = $("#carousel ul li").innerWidth;
-	            var ulCarousel = $("#carousel ul");
-	            //moveElement(ulCarousel, width);
-	            moveLastBeforeFirst(ulCarousel);
+	            MovePrevious();
 	            t = setInterval(MoveNext, 7500);
 	        });
 	    }
